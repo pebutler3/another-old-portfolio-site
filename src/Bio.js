95d0b80253b5138b8306c80532ec40fb4fc982ad
pebutler3 <p.e.butler3@gmail.com>
2016-09-07 16:25:29 -0600

    cleanend up

--- src/Bio.js
+++ src/Bio.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import Tools from './Tools';
+
 
 class Bio extends Component {
   render() {
     return (
-      <ul className="bio">
-        <img src={this.props.avatar} alt="" />
-        <h2>Bio</h2>
-        <li>Design + Code</li>
-        <li>Born outside of Dayton, Ohio and currently residing in Denver, Colorado.</li>
-        <li>Vim is my editor and chief.</li>
-        <li>I have a {this.props.age} year old little girl.</li>
-        <li>And a hot lady to tie it all together.</li>
-      </ul>
+      <div className="myInformation">
+        <img className="avatar" src={this.props.avatar} alt="P.E. Butler III" />
+        <ul className="bio">
+          <h2>Bio</h2>
+          <li>Design + Code</li>
+          <li>Born outside of Dayton, Ohio and currently residing in Denver, Colorado.</li>
+          <li>Vim is my editor and chief.</li>
+          <li>I have a {this.props.daughtersAge} year old little girl.</li>
+          <li>And a hot lady to tie it all together.</li>
+          <br />
+          <li><strong>Public Repos:</strong> {this.props.repos}</li>
+          <li>
+            <strong>Favorite Team:</strong> {this.props.myTeam}
+            <img className="fulhamCrest" src={this.props.crest} width="25" alt={this.props.myTeam}/>
+          </li>
+        </ul>
+        <Tools />
+      </div>
     );
   }
 }
