3b2b1d712b1542b41265e660ae99ca865741e0ac
pebutler3 <p.e.butler3@gmail.com>
2016-09-07 16:20:35 -0600

    created footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <ul>
+          <li className="name">P.E. Butler III</li>
+        </ul>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
