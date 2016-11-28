bd65c4c8a707304e5b4afb36213e736d623232da
pebutler3 <p.e.butler3@gmail.com>
2016-11-28 16:20:28 -0700

    Added indevelopment // added stack info

--- src/Projects.js
+++ src/Projects.js
@@ -3,17 +3,27 @@ import React, { Component } from 'react';
 class Projects extends Component {
   render() {
     let recentProjects = [
-      ['Roadmap', 'https://github.com/pebutler3/roadmap'],
-      ['Heron', 'https://github.com/getblackboard/heron'],
-      ['Wiki Search', 'http://codepen.io/pebutler3/pen/AXkJzy'],
-      ['Minimal Theme', 'https://minimal.mybrightsites.com/']
+      ['Roadmap', 'https://github.com/pebutler3/roadmap', 'React'],
+      ['Heron', 'https://github.com/getblackboard/heron', 'SCSS/Flexbox'],
+      ['Wiki Search', 'http://codepen.io/pebutler3/pen/AXkJzy', 'SCSS/JS/Jquery'],
+      ['Minimal Theme', 'https://minimal.mybrightsites.com/','HTML/CSS/JS/Liquid']
     ]
+
+    let inDevelopment = [
+      ['CAP Management', 'http://www.capmanagement.com','Wordpress']
+    ]
+
     return (
       <div className="col-3 projects">
         <h2>Recent Projects</h2>
         <ul className="recent-projects">
-          {recentProjects.map(project => <li key={project[1]}><a href={project[1]}>	&#8961; {project[0]}</a></li>)}
+          {recentProjects.map(project => <li key={project[1]}><a href={project[1]}>	{project[0]} - <small>{project[2]}</small></a></li>)}
         </ul>
+        <br />
+        <h2>In Development</h2> 
+          <ul className="recent-projects">
+            {inDevelopment.map(project => <li key={project[1]}><a href={project[1]}>	{project[0]} - <small>{project[2]}</small></a></li>)}
+          </ul>
       </div>
     );
   }
